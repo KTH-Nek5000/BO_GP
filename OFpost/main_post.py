83cf5a78561ed4a172543270cda696528e511f20
Yuki Morita <morita@b-an01.hpc2n.umu.se>
2019-12-20 12:42:05 +0100

    main_post.py: update save_beta

--- OFpost/main_post.py
+++ OFpost/main_post.py
@@ -25,7 +25,8 @@ import matplotlib.pyplot as plt
 import postProcess_func
 
 # default setting for figures
-#from matplotlib import rc
+from matplotlib import rc
+plt.rcParams["font.size"] = 20
 #rc('text', usetex=True)
 #plt.rcParams['font.family'] = 'Times New Roman'
 #plt.rcParams['xtick.direction'] = 'in'
@@ -57,7 +58,7 @@ def write_newTheta(obj):
     scf.write('%g' % obj)
     scf.close()
 
-def save_beta(saveFigPath,iMain,x,beta,delta99_in,in_exc,out_exc,beta_t):
+def save_beta(saveFigPath,iMain,x,beta,delta99_in,in_exc,out_exc,beta_t,obj):
     n=len(beta)
     plt.plot(x[1:-1], beta)
     #plt.xlabel(r'$x/\delta_{99}^{in}$')
@@ -65,14 +66,15 @@ def save_beta(saveFigPath,iMain,x,beta,delta99_in,in_exc,out_exc,beta_t):
     xmax=x[-1]
     ymin=-0.05 # set depends on your beta_t
     ymax=0.05
-    plt.plot([x[int(n*in_exc)+1],x[int(n*in_exc)+1]],[ymin,ymax],'k-.')
-    plt.plot([x[-int(n*out_exc)-1],x[-int(n*out_exc)-1]],[ymin,ymax],'k-.')
-    plt.hlines([beta_t],xmin,xmax,'r',linestyles='dashed')
+    plt.vlines(x[int(n*in_exc)+1],ymin,ymax,'k',linestyles='dashdot')
+    plt.vlines(x[-int(n*out_exc)-1],ymin,ymax,'k',linestyles='dashdot')
+    plt.hlines(beta_t,xmin,xmax,'r',linestyles='dashed')
     plt.xlabel(r'$x$')
     plt.ylabel(r'$\beta$')
     plt.xlim(xmin,xmax)
     plt.ylim(ymin,ymax)
     plt.grid()
+    plt.title(r'$N_i = %d, y = %f$' % (iMain,obj))
     saveFileName = "beta_%02d"% iMain
     plt.savefig(saveFigPath + saveFileName + ".pdf",bbox_inches="tight")
     print("save beta figureas %s%s.pdf" % (saveFigPath,saveFileName))
@@ -99,17 +101,15 @@ if __name__ == '__main__':
     
     # calc beta
     print("################### calc beta ####################")
-    x, beta = calc_beta(path2run,casename,U_infty,delta99_in,Nx,Ny,Nz,t)
-    
-    # save beta
-    save_beta(saveFigPath,iMain,x,beta,delta99_in,inlet_exclude,outlet_exclude,beta_t)
+    x, beta = calc_beta(path2run,casename,U_infty,delta99_in,Nx,Ny,Nz,t)    
     
     # assess objective func
     print("################### calc objective ####################")
     n = len(beta)
-    #print("len(beta) =", len(beta[int(inlet_exclude*n):-int(outlet_exclude*n)]))
-    #print(beta[int(inlet_exclude*n):-int(outlet_exclude*n)] - beta_t)
     obj = np.linalg.norm(beta[int(inlet_exclude*n):-int(outlet_exclude*n)] - beta_t) # L2norm
+    
+    # save beta
+    save_beta(saveFigPath,iMain,x,beta,delta99_in,inlet_exclude,outlet_exclude,beta_t,obj)
     
     # output obj
     print("objective = ",obj)
