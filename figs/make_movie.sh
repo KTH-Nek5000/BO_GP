83fff9913c9c63a09367a8d5cbbcc07efa049a73
Yuki Morita <morita@kth.se>
2020-01-18 21:16:17 +0100

    add figs/make_movie.sh

---
+++ figs/make_movie.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -eu
+
+# input check (obsolete)
+#if [ $# -ne 1 ]; then
+#    echo "give 1 augment: beta or gp1D" 1>&2
+#    exit 1
+#fi
+
+outputFileName="opt_hist"
+figs=("beta" "gp2D")
+
+for fig in ${figs[@]}; do
+    files="$fig*.pdf" # take list of files
+    # pdf to png
+    for file in $files; do
+	filename=`basename $file .pdf` # delete extension, .pdf
+	echo "convert $filename.pdf to $filename.png"
+	pdftoppm -png $file png/$filename # pdf to png
+	wait
+	mv png/${filename}-1.png png/$filename.png # rename
+    done
+    echo "############### make $fig.avi from .png files###############"
+    ffmpeg -y -framerate 0.5 -i png/${fig}_%02d.png png/tmp.avi # overwrite, 1 pic/sec
+    wait
+    echo "############# rescaling#################"
+    ffmpeg -y -i png/tmp.avi -vf scale=800:600 png/$fig.avi # rescale
+    rm png/tmp.avi
+done
+
+echo "################### png/${figs[0]}.avi + png/${figs[1]}.avi -> png/$outputFileName.avi ####################"
+ffmpeg -y -i png/${figs[0]}.avi -i png/${figs[1]}.avi -filter_complex "hstack" png/$outputFileName.avi
+#ffmpeg -y -i png/${figs[0]}.avi -i png/${figs[1]}.avi -filter_complex "nullsrc=size=1600x600 [base]; [0:v] setpts=PTS-STARTPTS, scale=800x600 [left]; [1:v] setpts=PTS-STARTPTS, scale=800x600 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=800" -c:v libx264 -r 1 png/$outputFileName.mkv
