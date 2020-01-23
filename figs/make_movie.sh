2d6c265c8b3e2f902217a7391d6176c5b5e1f4cb
Yuki Morita <morita@kth.se>
2020-01-23 14:18:19 +0100

    include yTop figures

--- figs/make_movie.sh
+++ figs/make_movie.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -eu
+###########
+#figure numbers should be 2 digits
+###########
 
 # input check (obsolete)
 #if [ $# -ne 1 ]; then
@@ -8,10 +11,10 @@ set -eu
 #fi
 
 outputFileName="opt_hist"
-figs=("beta" "gp2D")
+figs=("beta" "gp2D" "yTop")
 
 for fig in ${figs[@]}; do
-    files="$fig*.pdf" # take list of files
+    files="$fig*.pdf" # take list of pdf files
     # pdf to png
     for file in $files; do
 	filename=`basename $file .pdf` # delete extension, .pdf
@@ -28,6 +31,11 @@ for fig in ${figs[@]}; do
     rm png/tmp.avi
 done
 
-echo "################### png/${figs[0]}.avi + png/${figs[1]}.avi -> png/$outputFileName.avi ####################"
-ffmpeg -y -i png/${figs[0]}.avi -i png/${figs[1]}.avi -filter_complex "hstack" png/$outputFileName.avi
+# 2 figures
+#echo "################### png/${figs[0]}.avi + png/${figs[1]}.avi -> png/$outputFileName.avi ####################"
+#ffmpeg -y -i png/${figs[0]}.avi -i png/${figs[1]}.avi -filter_complex "hstack" png/$outputFileName.avi
+# 3 figures
+echo "################### png/${figs[0]}.avi + png/${figs[1]}.avi + png/${figs[2]}.avi -> png/$outputFileName.avi ####################"
+ffmpeg -y -i png/${figs[0]}.avi -i png/${figs[1]}.avi -i png/${figs[2]}.avi -filter_complex "[0:v][1:v][2:v]hstack=inputs=3[v]" -map "[v]" png/$outputFileName.avi
+# obsolete
 #ffmpeg -y -i png/${figs[0]}.avi -i png/${figs[1]}.avi -filter_complex "nullsrc=size=1600x600 [base]; [0:v] setpts=PTS-STARTPTS, scale=800x600 [left]; [1:v] setpts=PTS-STARTPTS, scale=800x600 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=800" -c:v libx264 -r 1 png/$outputFileName.mkv
